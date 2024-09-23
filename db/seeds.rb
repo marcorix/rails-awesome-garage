c8fb9eafb6af7db6b5bb2ec9099b2e08d527fcdf
Marco Ricci <mr.ricci79@gmail.com>
2024-09-23 19:28:49 +0200

    styled the fovourites page

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,6 @@ owner2 = Owner.create!(nickname: "Paulking79")
 puts "Creating cars.."
 
 car1 = Car.create!(brand: "Ferrari", model: "Testarossa", year: 1995, fuel: "Gasoline", owner: owner1, image_url: "https://www.clubalfa.it/wp-content/uploads/2020/12/Ferrari-Testarossa-storia-15.jpg")
-car2 = Car.create!(brand: "Mercedes", model: "EQS Suv", year: 2024, fuel: "Electic", owner: owner2, image_url: "https://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/14/2023/01/Mercedes-4.jpg")
+car2 = Car.create!(brand: "Mercedes", model: "EQS Suv", year: 2024, fuel: "Electric", owner: owner2, image_url: "https://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/14/2023/01/Mercedes-4.jpg")
 
 puts "Done! #{Owner.count} owners and #{Car.count} cars created."
